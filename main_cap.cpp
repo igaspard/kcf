c26fa04e629738a8edb60de1acb90589b687cf2a
Gaspard Shen <syuyang@gmail.com>
2019-07-01 12:19:09 +0800

    Camera input

--- main_cap.cpp
+++ main_cap.cpp
@@ -8,6 +8,29 @@
 using namespace std;
 using namespace cv;
 
+bool bRectLoaded = false;
+void Callback(int event, int x, int y, int flags, void* userdata) {
+    static int count = 0;
+    if(count == 2) {
+        if(!bRectLoaded)
+            bRectLoaded = true;
+        return;
+    }
+
+    if(event == EVENT_LBUTTONDOWN) {
+        Rect& data = *(Rect*)(userdata);
+        if(count == 0) {
+            data.x = x;
+            data.y = y;
+        }
+        else if(count == 1) {
+            data.width = x - data.x;
+            data.height = y - data.y;
+        }
+        ++count;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     VideoCapture cap;
@@ -28,12 +51,16 @@ int main(int argc, char const *argv[])
     KCF_Tracker tracker;
     namedWindow("Image");
     Rect init_rect;
-    init_rect.x = 428;
-    init_rect.y = 272;
-    init_rect.width = 209;
-    init_rect.height = 273;
+    setMouseCallback("Image", Callback, &init_rect);
+    // init_rect.x = 428;
+    // init_rect.y = 272;
+    // init_rect.width = 209;
+    // init_rect.height = 273;
 
     Size videoSize = Size((int)cap.get(CV_CAP_PROP_FRAME_WIDTH),(int)cap.get(CV_CAP_PROP_FRAME_HEIGHT));
+    cout << "Cap Size: " << videoSize << ", FOURCC: " << cap.get(CV_CAP_PROP_FOURCC)
+    << ", FPS: " << cap.get(CV_CAP_PROP_FPS) <<  endl;
+    
     VideoWriter writer;
     writer.open("output.mp4", cap.get(CV_CAP_PROP_FOURCC), cap.get(CV_CAP_PROP_FPS), videoSize);
     Mat frame;
@@ -47,26 +74,27 @@ int main(int argc, char const *argv[])
 			cout << "Frame empty, exit...\n";
 			break;
 		}
-        if (!init_success) {
+        if (bRectLoaded) {
+            if (!init_success) {
             cout << "tracker init" << endl;
             tracker.init(frame, init_rect);
             rectangle(frame, init_rect, Scalar( 0, 255, 0 ), 1, 8);
             init_success = true;
-        }
-        else {
-            cout << "Keep tracking " << endl;
-            double time_profile_counter = cv::getCPUTickCount();
-            tracker.track(frame);
-            time_profile_counter = cv::getCPUTickCount() - time_profile_counter;
-            avg_time += time_profile_counter/((double)cvGetTickFrequency()*1000);
-            frames++;
-            bb = tracker.getBBox();
-            rectangle(frame, cv::Rect(bb.cx - bb.w/2., bb.cy - bb.h/2., bb.w, bb.h), Scalar(0, 255, 0), 1, 8);
+            }
+            else {
+                cout << "Keep tracking " << endl;
+                double time_profile_counter = cv::getCPUTickCount();
+                tracker.track(frame);
+                time_profile_counter = cv::getCPUTickCount() - time_profile_counter;
+                avg_time += time_profile_counter/((double)cvGetTickFrequency()*1000);
+                frames++;
+                bb = tracker.getBBox();
+                rectangle(frame, cv::Rect(bb.cx - bb.w/2., bb.cy - bb.h/2., bb.w, bb.h), Scalar(0, 255, 0), 1, 8);
+            }
         }
         writer << frame;
         imshow("Image", frame);
         waitKey(1);
-        
     }
     cout << "Average processing speed " << avg_time/frames <<  "ms. (" << 1./(avg_time/frames)*1000 << " fps)" << endl;
     return 0;
